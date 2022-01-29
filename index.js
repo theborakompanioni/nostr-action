428e1de4bb8587964e69ff897ea461fa54d5755a
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-30 00:42:54 +0100

    feat: test for established connection

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-const core = require('@actions/core');
+const core = require('@actions/core')
 const nostr = require('./nostr-tools-commonjs')
+const wait = require('./wait')
 
-
-async function sendEvent(relay, eventObject) {
-  const pool = nostr.relayPool()
-  pool.setPolicy('wait', true);
-  pool.addRelay(relay, {read: false, write: true})
-
-  return new Promise((resolve, reject) => {
-    setTimeout(async () => {
-      try {
-        const ev = await pool.publish(eventObject, (status, url) => {
-          if (status === 0) {
-            console.log(`publish request sent to ${url}`)
-          }
-          if (status === 1) {
-            console.log(`event published by ${url}`, ev)
-          }
-        });
-        
-        resolve(ev);
-
-      } catch(err) {
-        console.log("Error while sending event: " + err);
-        reject(err)
-      } finally {
-        pool.removeRelay(relay)
-      }
-    }, 3000);
-  })
+const waitFor = async (testFn, maxTries = 100, waitDuration = 100) => {
+  for (let i = 0; i < maxTries - 1; i++) {
+    if (testFn()) {
+      return true
+    } else {
+      await wait(waitDuration)
+    }
+  }
+  return testFn()
 }
 
-const die = (msg) => { throw  new Error(msg) };
+const sendEvent = async (relayUrl, eventObject) => {
+  const pool = nostr.relayPool()
+  pool.setPolicy('wait', true)
 
-// most @actions toolkit packages have async methods
+  const relay = pool.addRelay(relayUrl, {read: false, write: true})
+
+  try {
+    const relayReady = await waitFor(() => relay.status === 1)
+    if (!relayReady) {
+      throw new Error(`Could not establish connection to relay ${relayUrl}`)
+    }
+
+    return await pool.publish(eventObject)
+  } finally {
+    pool.removeRelay(relayUrl)
+  }
+}
+
+const die = (msg) => { throw  new Error(msg) }
+
 async function run() {
   try {
-    // const relay = core.getInput('relay') || die('`relay` must not be empty');
-    // const content = core.getInput('content') || die('`content` must not be empty');
-    // const key = core.getInput('key') || die('`key` must not be empty');
-    const relay = core.getInput('relay') || 'wss://relayer.fiatjaf.com';
-    const content = core.getInput('content') || 'test';
-    const key = core.getInput('key') || 'bda95ba67b8f225d77858fc599cfc1e91b00e035c3e06c687b7e91ab1f22b895';
+    const relay = core.getInput('relay') || die('`relay` must not be empty')
+    const content = core.getInput('content') || die('`content` must not be empty')
+    const key = core.getInput('key') || die('`key` must not be empty')
 
     console.debug('Creating event..')
     const eventObject = nostr.getBlankEvent()
@@ -49,25 +45,24 @@ async function run() {
     eventObject.pubkey = Buffer.from(nostr.getPublicKey(key)).toString('hex')
     eventObject.content = content
     eventObject.tags = []
-    eventObject.created_at = Math.round(Date.now() / 1000);
+    eventObject.created_at = Math.round(Date.now() / 1000)
 
     console.debug('Signing event..')
-    eventObject.id = nostr.getEventHash(eventObject);
-    const sig = Buffer.from(await nostr.signEvent(eventObject, key)).toString('hex');
-    eventObject.sig = sig;
+    const sig = Buffer.from(await nostr.signEvent(eventObject, key)).toString('hex')
+    eventObject.sig = sig
+    eventObject.id = nostr.getEventHash(eventObject)
 
     console.debug('Validating event..')
-    nostr.validateEvent(eventObject) || die('event is not valid');
+    nostr.validateEvent(eventObject) || die('event is not valid')
 
     console.debug('Sending event..')
-    const event = await sendEvent(relay, eventObject);
-    console.log(event);
+    const event = await sendEvent(relay, eventObject)
+    console.log(event)
     
-    const eventHash = nostr.getEventHash(event);
-    core.setOutput('eventHash', JSON.stringify(eventHash));
+    core.setOutput('event', JSON.stringify(event))
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
