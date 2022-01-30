ec445bb8876b7e02a5782ce07bb311beeee66b74
theborakompanioni <theborakompanioni+github@gmail.com>
2022-01-30 01:52:38 +0100

    build: fix workflows

--- index.test.js
+++ index.test.js
@@ -18,10 +18,10 @@ test('wait 500 ms', async () => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_DRY'] = true;
-  process.env['INPUT_RELAY'] = 'wss://relayer.fiatjaf.com';
-  process.env['INPUT_CONTENT'] = 'test';
   // test key taken from https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#test-vector-4
   process.env['INPUT_KEY'] = '3ddd5602285899a946114506157c7997e5444528f3003f6134712147db19b678';
+  process.env['INPUT_RELAY'] = 'wss://relayer.fiatjaf.com';
+  process.env['INPUT_CONTENT'] = 'test';
   
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
