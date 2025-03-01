6241896eea078db790442f345bb96d17206ab195
theborakompanioni <theborakompanioni+github@gmail.com>
2025-03-01 17:49:39 +0100

    chore: export run in main.js

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,107 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 8415:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const core = __nccwpck_require__(7484)
+const WebSocket = __nccwpck_require__(1354)
+const { finalizeEvent, verifyEvent } = __nccwpck_require__(510)
+const { Relay, useWebSocketImplementation } = __nccwpck_require__(9843)
+const { hexToBytes } = __nccwpck_require__(4248)
+
+useWebSocketImplementation(WebSocket)
+
+const _sendEvent = (dryRun = false) => (async (relayUrl, eventObject) => {
+  core.debug(`Connecting to relay ${relayUrl}..`)
+
+  let relay
+  try {
+    relay = await Relay.connect(relayUrl)
+    core.debug(`Successfully connected to relay ${relayUrl}`)
+
+    if (!dryRun) {
+      await relay.publish(eventObject)
+    }
+
+    return eventObject
+  } catch (e) {
+    throw new Error(`Could not establish connection to relay ${relayUrl}: ${e.message || 'Unknown reason'}.`)
+  } finally {
+    core.debug(`Disconnecting from relay ${relayUrl}..`)
+    relay && relay.close()
+    core.debug(`Disconnected from relay ${relayUrl}`)
+  }
+})
+
+const sendEvent = _sendEvent()
+const sendEventDry = _sendEvent(true)
+
+const die = (msg) => { throw new Error(msg) }
+
+const DEFAULT_EVENT_TEMPLATE = {
+  kind: 1,
+  tags: [],
+  created_at: Math.floor(Date.now() / 1_000),
+}
+const DEFAULT_EVENT_TEMPLATE_STRING = JSON.stringify(DEFAULT_EVENT_TEMPLATE)
+
+const run = async () => {
+  try {
+    const relay = core.getInput('relay', { required: true })
+    const content = core.getInput('content', { required: true })
+    const key = core.getInput('key', { required: true })
+    const eventTemplateString = core.getInput('event_template', { required: false })
+    const dry = core.getBooleanInput('dry', { required: false })
+
+    core.setSecret(key)
+
+    const keyRaw = hexToBytes(key)
+
+    if (dry) {
+      core.info('dry-run enabled - connection to relays will be established, but no event will be sent.')
+    }
+
+    const eventTemplate = JSON.parse(eventTemplateString || DEFAULT_EVENT_TEMPLATE_STRING)
+    const validEventTemplate = typeof eventTemplate === 'object'
+      && !Array.isArray(eventTemplate)
+      && eventTemplate !== null
+    if (!validEventTemplate) {
+      throw new Error(`Could not build event from template: "${eventTemplateString}"`);
+    }
+
+    core.debug('Creating event..')
+    const rawEvent = {
+      kind: eventTemplate.kind ?? DEFAULT_EVENT_TEMPLATE.kind,
+      tags: eventTemplate.tags ?? DEFAULT_EVENT_TEMPLATE.tags,
+      created_at: eventTemplate.created_at ?? DEFAULT_EVENT_TEMPLATE.created_at,
+      content,
+    }
+
+    core.debug('Signing event..')
+    const eventObject = finalizeEvent(rawEvent, keyRaw)
+
+    core.debug('Validating event..')
+    verifyEvent(eventObject) || die('event is not valid')
+    
+    core.debug('Sending event..', dry ? '(dry-run enabled: event will not be sent)' : '')
+    const event = dry ? await sendEventDry(relay, eventObject) : await sendEvent(relay, eventObject)
+    core.debug('Successfully sent event.')
+    
+    core.setOutput('event', JSON.stringify(event))
+  } catch (error) {
+    const reason = error instanceof Error ? error : error.message || 'Unknown reason.'
+    core.setFailed(reason)
+  }
+}
+
+module.exports = {
+  run
+} 
+
+
+/***/ }),
+
 /***/ 4914:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -18032,96 +18133,7 @@ var Relay = class extends AbstractRelay {
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-const core = __nccwpck_require__(7484)
-const index_WebSocket = __nccwpck_require__(1354)
-const { finalizeEvent, verifyEvent } = __nccwpck_require__(510)
-const { Relay, useWebSocketImplementation } = __nccwpck_require__(9843)
-const { hexToBytes } = __nccwpck_require__(4248)
-
-useWebSocketImplementation(index_WebSocket)
-
-const _sendEvent = (dryRun = false) => (async (relayUrl, eventObject) => {
-  core.debug(`Connecting to relay ${relayUrl}..`)
-
-  let relay
-  try {
-    relay = await Relay.connect(relayUrl)
-    core.debug(`Successfully connected to relay ${relayUrl}`)
-
-    if (!dryRun) {
-      await relay.publish(eventObject)
-    }
-
-    return eventObject
-  } catch (e) {
-    throw new Error(`Could not establish connection to relay ${relayUrl}: ${e.message || 'Unknown reason'}.`)
-  } finally {
-    core.debug(`Disconnecting from relay ${relayUrl}..`)
-    relay && relay.close()
-    core.debug(`Disconnected from relay ${relayUrl}`)
-  }
-})
-
-const sendEvent = _sendEvent()
-const sendEventDry = _sendEvent(true)
-
-const die = (msg) => { throw new Error(msg) }
-
-const DEFAULT_EVENT_TEMPLATE = {
-  kind: 1,
-  tags: [],
-  created_at: Math.floor(Date.now() / 1_000),
-}
-const DEFAULT_EVENT_TEMPLATE_STRING = JSON.stringify(DEFAULT_EVENT_TEMPLATE)
-
-async function run() {
-  try {
-    const relay = core.getInput('relay', { required: true })
-    const content = core.getInput('content', { required: true })
-    const key = core.getInput('key', { required: true })
-    const eventTemplateString = core.getInput('event_template', { required: false })
-    const dry = core.getBooleanInput('dry', { required: false })
-
-    core.setSecret(key)
-
-    const keyRaw = hexToBytes(key)
-
-    if (dry) {
-      core.info('dry-run enabled - connection to relays will be established, but no event will be sent.')
-    }
-
-    const eventTemplate = JSON.parse(eventTemplateString || DEFAULT_EVENT_TEMPLATE_STRING)
-    const validEventTemplate = typeof eventTemplate === 'object'
-      && !Array.isArray(eventTemplate)
-      && eventTemplate !== null
-    if (!validEventTemplate) {
-      throw new Error(`Could not build event from template: "${eventTemplateString}"`);
-    }
-
-    core.debug('Creating event..')
-    const rawEvent = {
-      kind: eventTemplate.kind ?? DEFAULT_EVENT_TEMPLATE.kind,
-      tags: eventTemplate.tags ?? DEFAULT_EVENT_TEMPLATE.tags,
-      created_at: eventTemplate.created_at ?? DEFAULT_EVENT_TEMPLATE.created_at,
-      content,
-    }
-
-    core.debug('Signing event..')
-    const eventObject = finalizeEvent(rawEvent, keyRaw)
-
-    core.debug('Validating event..')
-    verifyEvent(eventObject) || die('event is not valid')
-    
-    core.debug('Sending event..', dry ? '(dry-run enabled: event will not be sent)' : '')
-    const event = dry ? await sendEventDry(relay, eventObject) : await sendEvent(relay, eventObject)
-    core.debug('Successfully sent event.')
-    
-    core.setOutput('event', JSON.stringify(event))
-  } catch (error) {
-    const reason = error instanceof Error ? error : error.message || 'Unknown reason.'
-    core.setFailed(reason)
-  }
-}
+const { run } = __nccwpck_require__(8415)
 
 run()
 
